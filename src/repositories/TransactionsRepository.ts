232f3790e592aa6fc8f00e998860f3a2676634d6
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-08-10 23:46:36 -0300

    Add custom transactions repository to get balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,32 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const balance = transactions.reduce(
+      (balanceAccumulator, transaction) => {
+        if (transaction.type === 'income') {
+          // eslint-disable-next-line no-param-reassign
+          balanceAccumulator.income += Number(transaction.value);
+        } else {
+          // eslint-disable-next-line no-param-reassign
+          balanceAccumulator.outcome += Number(transaction.value);
+        }
+
+        // eslint-disable-next-line no-param-reassign
+        balanceAccumulator.total =
+          balanceAccumulator.income - balanceAccumulator.outcome;
+
+        return balanceAccumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 }
 
